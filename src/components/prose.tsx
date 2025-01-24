4eabd3ef51da5a3c09bec761c4e540b59211f65b
dalton <dakribe94@gmail.com>
2025-01-23 18:16:02 -0600

    remove solid start

--- src/components/prose.tsx
+++
@@ -1,9 +0,0 @@
-import { JSX } from "solid-js";
-
-export default function Prose(props: { children: JSX.Element }) {
-	return (
-		<div class="prose prose-pre:m-0 dark:prose-invert prose-zinc prose-pre:rounded-none prose-pre:rounded-b-md min-w-full prose-code:bg-[#232137] prose-pre:bg-[#232137]">
-			{props.children}
-		</div>
-	);
-}
--- src/data/posts.ts
+++
@@ -1,7 +0,0 @@
-import JSONPosts from "./posts.json";
-type Post = {
-	title: string;
-	date: string;
-	slug: string;
-};
-export const posts: Post[] = JSONPosts;
