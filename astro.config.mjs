366e547f31536c3bbfd84930441483d6cb8ed5d3
dalton <dakribe94@gmail.com>
2025-02-07 07:45:43 -0600

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,21 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		shikiConfig: {
+			theme: "one-light",
+		},
+	},
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	output: "server",
+	adapter: vercel(),
 });
+
