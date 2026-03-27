839d67acae3c427f011b560e4bb178d7a645cfc5
dakribe <dakribe94@gmail.com>
2026-03-27 18:20:27 -0500

    topbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,15 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
 import "./app.css";
+import { Topbar } from "./components/topbar";
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
-          <Nav />
+          <Topbar />
           <Suspense>{props.children}</Suspense>
         </>
       )}
